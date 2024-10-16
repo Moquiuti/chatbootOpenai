553a5c1a122dff8632368cd419a40297c74cead1
leandro.ferreira <leandro.ferreira@monitoratec.com.br>
2024-10-16 16:56:58 -0300

    Integrando com a API

---
+++ src/main/java/br/com/alura/ecomart/chatbot/domain/service/ChatBootService.java
@@ -0,0 +1,22 @@
+package br.com.alura.ecomart.chatbot.domain.service;
+
+import br.com.alura.ecomart.chatbot.infra.openai.DadosRequisicaoChatCompletion;
+import br.com.alura.ecomart.chatbot.infra.openai.OpenAIClient;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatBootService {
+
+    private final OpenAIClient client;
+
+    public ChatBootService(OpenAIClient client) {
+        this.client = client;
+    }
+
+    public String responderPergunta(@NotNull final String pergunta) {
+        final var promptSistema = "Você é um chatbot de atendimento a clientes de um ecommerce e deve responder apenas perguntas relacionadas com o ecommerce";
+        final var dados = new DadosRequisicaoChatCompletion(promptSistema, pergunta);
+        return client.enviarRequisicaoChatCompletion(dados);
+    }
+}
--- src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
+++ src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecomart.chatbot.web.controller;
 
+import br.com.alura.ecomart.chatbot.domain.service.ChatBootService;
 import br.com.alura.ecomart.chatbot.web.dto.PerguntaDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,12 @@ public class ChatController {
 
     private static final String PAGINA_CHAT = "chat";
 
+    private final ChatBootService chatBootService;
+
+    public ChatController(ChatBootService chatBootService) {
+        this.chatBootService = chatBootService;
+    }
+
     @GetMapping
     public String carregarPaginaChatbot() {
         return PAGINA_CHAT;
@@ -18,7 +25,7 @@ public class ChatController {
     @PostMapping
     @ResponseBody
     public String responderPergunta(@RequestBody PerguntaDto dto) {
-        return dto.pergunta();
+        return chatBootService.responderPergunta(dto.pergunta());
     }
 
     @GetMapping("limpar")
