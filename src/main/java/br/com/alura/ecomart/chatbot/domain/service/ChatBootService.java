b76bb8265aab01c9ef602e9f51a10c5aa2b82978
leandro.ferreira <leandro.ferreira@monitoratec.com.br>
2024-10-17 10:03:35 -0300

    adicionado steam nas respostas, dando uma usabilidade para o usuário igual ao do chatgpt... as respostas(tokens) são entregues por partes...

--- src/main/java/br/com/alura/ecomart/chatbot/domain/service/ChatBootService.java
+++ src/main/java/br/com/alura/ecomart/chatbot/domain/service/ChatBootService.java
@@ -2,6 +2,8 @@ package br.com.alura.ecomart.chatbot.domain.service;
 
 import br.com.alura.ecomart.chatbot.infra.openai.DadosRequisicaoChatCompletion;
 import br.com.alura.ecomart.chatbot.infra.openai.OpenAIClient;
+import com.theokanning.openai.completion.chat.ChatCompletionChunk;
+import io.reactivex.Flowable;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,7 @@ public class ChatBootService {
         this.client = client;
     }
 
-    public String responderPergunta(@NotNull final String pergunta) {
+    public Flowable<ChatCompletionChunk> responderPergunta(@NotNull final String pergunta) {
         final var promptSistema = "Você é um chatbot de atendimento a clientes de um ecommerce e deve responder apenas perguntas relacionadas com o ecommerce";
         final var dados = new DadosRequisicaoChatCompletion(promptSistema, pergunta);
         return client.enviarRequisicaoChatCompletion(dados);
